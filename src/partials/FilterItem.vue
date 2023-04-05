7dbc2741708714ce891ab34243cbaf88df5463b0
bennieads-dot <benjamin.j.adams144@gmail.com>
2023-04-04 21:15:26 -0400

    changing filters to tailwind

--- src/partials/FilterItem.vue
+++ src/partials/FilterItem.vue
@@ -1,34 +1,5 @@
-<template>
-  <Combobox v-model="selected" multiple>
-    <ul v-if="selected.length > 0">
-      <li v-for="item in this.selected" :key="item.id">
-        {{ item.value }}
-      </li>
-    </ul>
-    <ComboboxInput />
-    <ComboboxOptions>
-      <ComboboxOption v-for="option in options" :key="option.id" :value="option.value">
-        {{ option.value }}
-      </ComboboxOption>
-    </ComboboxOptions>
-  </Combobox>
-</template>
+<template></template>
 
 <script>
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxOptions,
-  ComboboxOption,
-} from '@headlessui/vue'
 
-export default {
-  name: 'FilterItem',
-  props: ['options'],
-  data() {
-    return {
-      selected: []
-    }
-  }
-}
 </script>
--- src/partials/Filters.vue
+++ src/partials/Filters.vue
@@ -1,5 +1,44 @@
 <template>
-  <FilterItem v-for="filter in filters" :key="filter.id" :options="filter.values" />
+  <div class="flex overflow-x-scroll pb-10 hide-scroll-bar">
+    <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
+      <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white sr-only">Select an
+        option</label>
+      <select id="countries"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 inline-block">
+        <option selected>Choose a country</option>
+        <option value="US">United States</option>
+        <option value="CA">Canada</option>
+        <option value="FR">France</option>
+        <option value="DE">Germany</option>
+      </select>
+      <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white sr-only">Select an
+        option</label>
+      <select id="countries"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 min-w-max">
+        <option selected>Choose a country</option>
+        <option value="US">United States</option>
+        <option value="CA">Canada</option>
+        <option value="FR">France</option>
+        <option value="DE">Germany</option>
+      </select>
+      <select id="countries"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <option selected>Choose a country</option>
+        <option value="US">United States</option>
+        <option value="CA">Canada</option>
+        <option value="FR">France</option>
+        <option value="DE">Germany</option>
+      </select>
+      <select id="countries"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <option selected>Choose a country</option>
+        <option value="US">United States</option>
+        <option value="CA">Canada</option>
+        <option value="FR">France</option>
+        <option value="DE">Germany</option>
+      </select>
+    </div>
+  </div>
 </template>
 <script>
 import FilterItem from '../partials/FilterItem.vue'
