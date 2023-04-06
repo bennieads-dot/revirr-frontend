2913a53bf9423c0b328dec909ced422d18ffe4fc
bennieads-dot <benjamin.j.adams144@gmail.com>
2023-04-05 22:34:17 -0400

    filter additions

--- src/partials/Filters.vue
+++ src/partials/Filters.vue
@@ -1,42 +1,7 @@
 <template>
-  <div class="flex overflow-x-scroll pb-10 hide-scroll-bar">
-    <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
-      <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white sr-only">Select an
-        option</label>
-      <select id="countries"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 inline-block">
-        <option selected>Choose a country</option>
-        <option value="US">United States</option>
-        <option value="CA">Canada</option>
-        <option value="FR">France</option>
-        <option value="DE">Germany</option>
-      </select>
-      <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white sr-only">Select an
-        option</label>
-      <select id="countries"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 min-w-max">
-        <option selected>Choose a country</option>
-        <option value="US">United States</option>
-        <option value="CA">Canada</option>
-        <option value="FR">France</option>
-        <option value="DE">Germany</option>
-      </select>
-      <select id="countries"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        <option selected>Choose a country</option>
-        <option value="US">United States</option>
-        <option value="CA">Canada</option>
-        <option value="FR">France</option>
-        <option value="DE">Germany</option>
-      </select>
-      <select id="countries"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        <option selected>Choose a country</option>
-        <option value="US">United States</option>
-        <option value="CA">Canada</option>
-        <option value="FR">France</option>
-        <option value="DE">Germany</option>
-      </select>
+  <div class="flex flex-row mt-2">
+    <div v-for="filter in filters" class="mr-2">
+      <FilterItem :name="filter.name" :values="filter.values" />
     </div>
   </div>
 </template>
@@ -50,12 +15,12 @@ export default {
   data() {
     return {
       filters: {
-        jobType: { id: 1, values: [{ id: 1, value: "Full-time" }, { id: 2, value: "Part-time" }, { id: 3, value: "Internship" }] },
+        jobType: { name: "jobType", id: 1, values: [{ id: 1, value: "Full-time" }, { id: 2, value: "Part-time" }, { id: 3, value: "Internship" }] },
         skills: {
-          id: 2, values: [{ id: 1, value: "Programming" }, { id: 2, value: "Design" }, { id: 3, value: "Management" }]
+          name: "Skills", id: 2, values: [{ id: 1, value: "Programming" }, { id: 2, value: "Design" }, { id: 3, value: "Management" }]
         },
-        salaryRange: { id: 3, values: [{ id: 1, value: "$20K-$50K" }, { id: 2, value: "$50K-$100K" }, { id: 3, value: ">$100K" }] },
-        location: { id: 4, values: [{ id: 1, value: "Baltimore" }, { id: 2, value: "Annapolis" }] },
+        salaryRange: { name: "SalaryRange", id: 3, values: [{ id: 1, value: "$20K-$50K" }, { id: 2, value: "$50K-$100K" }, { id: 3, value: ">$100K" }] },
+        location: { name: "location", id: 4, values: [{ id: 1, value: "Baltimore" }, { id: 2, value: "Annapolis" }] },
       }
     }
   }
